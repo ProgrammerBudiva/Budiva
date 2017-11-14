661c200bda51a2979ef7aff94f3d9e19ecc042b8
andrey <19ofis96@mail.ru>
2017-11-14 14:10:10 +0200

    hide 'print price' on tan price-list

--- wp-content/themes/budiva/parts/tabs/price.php
+++ wp-content/themes/budiva/parts/tabs/price.php
@@ -8,7 +8,7 @@
 <a href="<?= $url; ?>" class="tab-price-btn view" target="_blank">Просмотреть прайс</a>
 <a href="<?= $url; ?>" class="tab-price-btn download" download>Скачать прайс</a>
 <?php /* <a href="#" class="tab-price-btn send">Переслать на почту</a> */ ?>
-<a href="#" class="tab-price-btn print">Распечатать</a>
+<!--<a href="#" class="tab-price-btn print">Распечатать</a>-->
 
 <script>
     $(document).ready(function() {
