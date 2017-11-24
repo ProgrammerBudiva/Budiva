ba7a564e9548ffd9018f900d64d8ee86822de877
andrey <19ofis96@mail.ru>
2017-11-24 10:20:37 +0200

    скрыть "показать" на кнопках в новостях

--- wp-content/themes/budiva/parts/news-stock.php
+++ wp-content/themes/budiva/parts/news-stock.php
@@ -1,13 +1,13 @@
 <div class="tabs-navigation visible-xs visible-sm visible-md">
     <div class="container">
         <div class="col-xs-6">
-            показать <br>
+<!--            показать <br>-->
             <a href="<?= get_home_url(); ?>/novosti/" class="tabs-butt">
                 Новости
             </a>
         </div>
         <div class="col-xs-6">
-            показать <br>
+<!--            показать <br>-->
             <a href="<?= get_home_url(); ?>/aktsii/" class="tabs-butt">
                 Акции
             </a>
