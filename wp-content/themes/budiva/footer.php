f66225959cd8a8d001f2c718251e0f6d02e81ec3
andrey <19ofis96@mail.ru>
2018-01-09 12:26:40 +0200

    test phone click additional menu on catalog 2

--- wp-content/themes/budiva/footer.php
+++ wp-content/themes/budiva/footer.php
@@ -175,7 +175,7 @@
             });
 
             $(document).mouseup(function (e){ // событие клика по веб-документу
-                var div = $(".additional-menu"); // тут указываем ID элемента
+                var div = $(".additional-menu-box"); // тут указываем ID элемента
                 if (!div.is(e.target) // если клик был не по нашему блоку
                     && div.has(e.target).length === 0) { // и не по его дочерним элементам
 //                    div.hide(); // скрываем его
