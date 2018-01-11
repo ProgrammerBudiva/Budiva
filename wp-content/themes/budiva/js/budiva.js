a4873543898790d66d40a100e6934c37c053fee8
andrey <19ofis96@mail.ru>
2018-01-11 17:13:58 +0200

    test 1click GA event

--- wp-content/themes/budiva/js/budiva.js
+++ wp-content/themes/budiva/js/budiva.js
@@ -288,5 +288,6 @@ document.addEventListener( 'wpcf7mailsent', function( event ) {
 
         $('#1click').append('<div class="mini-popup-1click"><p>Ваша заявка отправлена!</p></div>')
         setTimeout(function(){$('.mini-popup-1click').fadeOut('fast')},5000);
+        _gaq.push(['_trackEvent', 'Buy', '1click']);
     }
 }, false );
